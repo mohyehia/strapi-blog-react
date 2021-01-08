517010be4316d16c5c21f4d6de810c23fb523c20
Mohamed.Yehia <mohamed.yehia>
2021-01-08 20:13:16 +0200

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import {Container, CssBaseline} from "@material-ui/core";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Strapi Blog</h1>
-        </div>
+        <BrowserRouter>
+            <CssBaseline/>
+            <Navbar/>
+            <Switch>
+                <Container maxWidth="lg">
+                    <Route path="/" component={Home} exact/>
+                </Container>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
